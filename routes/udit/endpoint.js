ceb3309c7c7898fc4802ceb3855ed1b9c6370293
ud17 <upandya1297@gmail.com>
2023-02-05 00:00:33 -0500

    register-admin route in progress

--- routes/udit/endpoint.js
+++ routes/udit/endpoint.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const Response = require("../../global/response");
+const Code = require("../../global/code");
+const { body, validationResult } = require('express-validator');
+
 const router = express.Router();
 
-router.get("/welcome", (req, res) => {
-    res.send("Working");
-})
+router.post("/register-admin", 
+
+    [
+        body("email").isEmail().withMessage("Email format is incorrect"),
+        body("password").isLength({min:8, max:20}).withMessage("Password should be 8 to 20 characters long"),
+        body("name").notEmpty().withMessage("Parameter `name` missing.")
+    ],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+
+            return Response.error(
+                res,
+                Code.BAD_REQUEST,
+                errors.array().map((error) => ({
+                    field: error.param,
+                    errorMessage: error.msg,
+                }))
+            );
+        }
+
+
+    }
+);
 
 module.exports = router;
